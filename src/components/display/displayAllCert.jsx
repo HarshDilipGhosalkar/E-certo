ba03bf80ca345a49d3e4261b0bfeca4501b1a06b
Jhenil Parihar <jhenilparihar@gmail.com>
2022-07-26 20:15:40 +0530

    format

--- src/components/display/displayAllCert.jsx
+++ src/components/display/displayAllCert.jsx
@@ -20,25 +20,19 @@ class DisplayCert extends Component {
   //   this.setState({allCert : this.props.allCert});
   // };
 
-  
   render() {
-    
- console.log("all cert0",this.props.cert1);
+    console.log("all cert", this.props.allCert);
     return (
       <>
         <div class="outer-container">
-          
           <div className="main-explore-container">
-           
             <div className="row nft-card-row">
               <div className="container">
                 <div class="row nft-container">
-                  
                   {this.props.allCert.length !== 0 ? (
                     <>
                       {this.props.allCert.map((cert) => {
                         return (
-                          
                           <div
                             key={cert.certid.toNumber()}
                             class="col-4 col-lg-4 col-md-6 col-sm-1 align-items-center nft_card"
@@ -46,26 +40,20 @@ class DisplayCert extends Component {
                             {/* <Link
                               to={"/assets/details/" + NFT.tokenId.toNumber()}
                             > */}
-                            
-                              <div className="details-div">
-                                
-                                <div class="row nft-details">
-                                  <div class="col nft-name-explore">
-                                    <p>hello</p>
-                                    <p class="n">
-                                    
-                                        {cert.certid.toNumber()}
-                                      
-                                    </p>
-                                    <p class="nft-owner-name-explore">
-                                      <p>jach</p>
-                                      {cert.certURI}
-                                    </p>
-                                  </div>
-                                 
+
+                            <div className="details-div">
+                              <div class="row nft-details">
+                                <div class="col nft-name-explore">
+                                  <p>hello</p>
+                                  <p class="n">{cert.certid.toNumber()}</p>
+                                  <p class="nft-owner-name-explore">
+                                    <p>jach</p>
+                                    {cert.certURI}
+                                  </p>
                                 </div>
-                                <div class="row buy-details"></div>
                               </div>
+                              <div class="row buy-details"></div>
+                            </div>
                             {/* </Link> */}
                           </div>
                         );
