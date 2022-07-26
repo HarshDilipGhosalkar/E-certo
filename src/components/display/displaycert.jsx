b986188e842621bf6a018aa8a5aa01202b0cfea9
Jhenil Parihar <jhenilparihar@gmail.com>
2022-07-26 23:23:04 +0530

    dynamic details display

--- src/components/display/displaycert.jsx
+++ src/components/display/displaycert.jsx
@@ -1,26 +1,19 @@
 import React from "react";
-// import NFTDetails from "./NFT-Details";
 import { useParams } from "react-router-dom";
 
-const DisplayCert = ({
-  AllCert
-}) => {
+const DisplayCert = ({ AllCert }) => {
   const { id } = useParams();
-  let certId = parseInt(id, 10) - 1;
-  let cert = AllCert[certId];
-  
+  const certId = parseInt(id, 10) - 1;
+  const cert = AllCert[certId];
   return (
     <>
-    {cert.metaData !== undefined ? (
-      <>
-      {/* <NFTDetails
-        cert={cert}
-      /> */}
-      <h1>{cert.certid.toNumber()}</h1>
-      <h1>{cert.metaData.name}</h1>
-      <h1>{cert.metaData.course}</h1>
-      </>
-        ) : null}
+      {cert !== undefined && cert.metaData !== undefined ? (
+        <>
+          <h1>{cert.certid.toNumber()}</h1>
+          <h1>{cert.metaData.name}</h1>
+          <h1>{cert.metaData.course}</h1>
+        </>
+      ) : null}
     </>
   );
 };
