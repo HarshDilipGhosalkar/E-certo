733529da823fc7eac93f325bf324d4edcfe7688d
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2022-07-27 15:47:54 +0530

    added print functionality

--- src/components/display/displaycert.jsx
+++ src/components/display/displaycert.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import QRCode from 'qrcode';
+import printJS from 'print-js'
 
 const DisplayCert = ({ AllCert }) => {
   const { id } = useParams();
   const certId = parseInt(id, 10) - 1;
   const linkCertId =parseInt(id, 10);
   const cert = AllCert[certId];
-  const [url, setUrl] = useState('http://localhost:3001/assets/details/'+ linkCertId)
-  // if (cert !== undefined && cert.metaData !== undefined){
-  //   setUrl('http://localhost:3001/assets/details/'+ cert.certid.toNumber())
+  const [url, setUrl] = useState('http://localhost:3000/assets/details/'+ linkCertId)
 
-  // }
- 
+  
 	const [qr, setQr] = useState('')
   const [mounted, setMounted] = useState(false)
-  
+ function onclickprint (){
+    // event.preventDefault();
+    // window.print();
+    printJS("printcertificate", "html");
+  };
   
 	// const GenerateQRCode = () => {
 	// 	QRCode.toDataURL(url, {
@@ -61,6 +63,7 @@ const DisplayCert = ({ AllCert }) => {
       {/* <NFTDetails
         cert={cert}
       /> */}
+      <div id="printcertificate">
       <h1>{cert.certid.toNumber()}</h1>
       <h1>{cert.transactionHash}</h1>
       <h1>{cert.metaData.name}</h1>
@@ -68,11 +71,26 @@ const DisplayCert = ({ AllCert }) => {
       <h1>{cert.metaData.contact}</h1>
       {qr && <>
 				<img width={200} height={200} src={qr} />
+        
         <br />
-				<a href={qr} download="qrcode.png">Download</a>
+				<a class="downloadcert" href={qr} download="qrcode.png">Download</a>
 			</>}
+      </div>
+      <br />
+      <button
+          className="btn"
+          onClick={onclickprint}
+          variant="success"
+          type="submit"
+          class="btns"
+        >
+          Print
+        </button>
+       
       </>
         ) : null}
+        
+        
     </>
   );
 };
