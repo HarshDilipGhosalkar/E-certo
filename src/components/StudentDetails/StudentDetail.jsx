4fca1a3a94a088925139a39ead0aef5fa5e79f3f
Jhenil Parihar <jhenilparihar@gmail.com>
2022-08-12 19:32:47 +0530

    display student details

--- src/components/StudentDetails/StudentDetail.jsx
+++ src/components/StudentDetails/StudentDetail.jsx
@@ -1,35 +1,27 @@
-import React from "react";  
+import React from "react";
 import { useParams } from "react-router-dom";
 
-const StudentDetail = ({
-  AllCert
-}) => {
+const StudentDetail = ({ AllCert }) => {
   const { hash } = useParams();
-  // const certId = parseInt(id, 10) - 1;
-  // const linkCertId =parseInt(id, 10);
-  // const cert = AllCert[certId];
-  let cert;
-  AllCert.forEach(c => {
-      if(c.transactionHash == hash){
-          cert =c;
-      }
+  let certificateDetail;
+  AllCert.forEach((cert) => {
+    if (cert.transactionHash == hash) {
+      certificateDetail = cert;
+    }
   });
+  console.log(certificateDetail);
   return (
     <>
-         {cert !== undefined && cert.metaData !== undefined? (
-      <>
-     
-      <div>
-      <h1>Name : {cert.metaData.name}</h1>
-      <h1>Deparment : {cert.metaData.course}</h1>
-      <h1>Phone : {cert.metaData.contact}</h1>
-     
-      </div>
-      <br />
-
-       
-      </>
-        ) : null}
+      {certificateDetail !== undefined ? (
+        <>
+          <div>
+            <h1>Name : {certificateDetail.name}</h1>
+            <h1>Deparment : {certificateDetail.course}</h1>
+            <h1>Phone : {certificateDetail.contact.toNumber()}</h1>
+          </div>
+          <br />
+        </>
+      ) : null}
     </>
   );
 };
