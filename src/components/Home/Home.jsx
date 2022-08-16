c4d70667566d77c6ddf7ad36372e3177652a47fc
Jhenil Parihar <jhenilparihar@gmail.com>
2022-08-16 23:34:43 +0530

    landing section created

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,47 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
+import "./assets/style.css";
+import banner from "./assets/image/banner.png";
+import create from "./assets/image/create.png";
+import issue from "./assets/image/issue.png";
+import manage from "./assets/image/manage.png";
+import share from "./assets/image/share.png";
+import verify from "./assets/image/verify.png";
 
 const Home = () => {
   return (
-    <div class="main">
-      <div class="head-container">
-        <div class="fading-div">
-          <div class="text-div">
-            <div class="text-area">
-              <h1>Discover, collect, and sell extraordinary NFTs</h1>
-              <span>
-                OpenSea is the world's first and <br /> largest NFT marketplace
-              </span>
-              <div class="btn-div">
-                <Link to="/marketplace">
-                  <button type="submit" id="explore-btn">
-                    Explore
-                  </button>
-                </Link>
-                <Link to="/mint">
-                  <button type="submit" id="create-btn">
-                    Create
-                  </button>
-                </Link>
+    <>
+      <section>
+        <div className="container">
+          <div className="hero">
+            <div className="row">
+              <div className="col-5 column text-area">
+                <h1 className="main-heading">
+                  Professional Certificate Maker - create and send certificates
+                </h1>
+                <p className="sub-heading">
+                  Our digital credentials infrastructure has everything you need
+                  to generate certificates. A certificate builder, templates,
+                  emails, security, and analytics.
+                </p>
+                <button className="start-btn">Get Started</button>
               </div>
-            </div>
-          </div>
-          <div class="banner-div">
-            <div class="nft-banner">
-              <div class="landing-img"></div>
-              <div class="landing-img-details">
-                <div class="landing-prof-img"></div>
-                <div class="landing-prof-img-det">
-                  <p class="name">Boared Ape</p>
-                  <p class="user-name">Bored Ape Yatch Club</p>
-                </div>
+              <div className="col-7">
+                <span className="banner-image-area">
+                  <img className="banner-image" src={banner} alt="" />
+                </span>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
