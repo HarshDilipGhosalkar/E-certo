7f3911ef57295bfa81b8822f22fbcf57ffb390dd
Jhenil Parihar <jhenilparihar@gmail.com>
2022-08-21 22:31:52 +0530

    modified student details page

--- src/components/StudentDetails/StudentDetail.jsx
+++ src/components/StudentDetails/StudentDetail.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./assets/styles.css";
+import logo from "./assets/sbmp-logo.png";
+import gif from "./assets/verified.gif";
+
 
 const StudentDetail = ({ AllCert }) => {
   const { hash } = useParams();
@@ -14,12 +18,36 @@ const StudentDetail = ({ AllCert }) => {
     <>
       {certificateDetail !== undefined ? (
         <>
-          <div>
+          {/* <div>
             <h1>Name : {certificateDetail.name}</h1>
             <h1>Deparment : {certificateDetail.course}</h1>
             <h1>Phone : {certificateDetail.contact.toNumber()}</h1>
           </div>
-          <br />
+          <br /> */}
+          <div className="details_page">
+            <div className="details_component">
+              <div className="details_grid">
+                <div className="details_div">
+                  <div className="detail_heading">
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-2">
+                          <img className="sbmp_logo" src={logo} alt="" />
+                        </div>
+                        <div className="col">
+                          <h2>Shri Bhagubhai Mafatlal Poltechnic</h2>
+                        </div>
+                      </div>
+                      <hr className="detail_hr" />
+                      <div className="verified_gif">
+                        <img src={gif} alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </>
       ) : null}
     </>
