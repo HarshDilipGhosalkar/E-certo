02c9b91e465b1dc99ccc0f569852e413bf0ea131
Jhenil Parihar <jhenilparihar@gmail.com>
2022-09-10 00:15:36 +0530

    created RecipientsList Component

---
+++ src/components/RecipientsList/RecipientsList.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import * as XLSX from "xlsx";
+
+// import "./assets/styles.css";
+
+class RecipientsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      excelData: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    await this.handleSubmit();
+  };
+
+  handleSubmit = async () => {
+    if (this.props.excelFile !== null) {
+      const workbook = XLSX.read(this.props.excelFile, { type: "buffer" });
+      const worksheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[worksheetName];
+      const data = XLSX.utils.sheet_to_json(worksheet);
+      this.setState({ excelData: data });
+
+      //   createBulkCertificate(data);
+    } else {
+      this.setState({ excelData: null });
+    }
+  };
+
+  render() {
+    console.log(this.state.excelData)
+    return (
+      <>
+        {this.props.excelFile != null ? (
+          <div className="excel_page">
+            <div className="excel_component">
+              <div className="upload_excel_header">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-6">
+                      <p className="path">
+                        <span className="light_fg">Certificate / </span> Upload
+                        Spreadsheet
+                      </p>
+                      <h5>Upload Spreadsheet</h5>
+                    </div>
+                    <div className="col-6">
+                      <div className="file_input_area">
+                        <br />
+                        <button>Reupload Spreadsheet</button>
+                        <button>Create and Publish Certificates</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="upload_excel_main"></div>
+            </div>
+          </div>
+        ) : (
+          <Navigate replace to="../createFromExel" />
+        )}
+      </>
+    );
+  }
+}
+
+export default RecipientsList;
