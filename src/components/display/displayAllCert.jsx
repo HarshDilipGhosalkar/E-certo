4a6b53e0dac2a538f8708d85b976400c06066f11
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2022-09-10 05:01:59 +0530

    built the structure

--- src/components/display/displayAllCert.jsx
+++ src/components/display/displayAllCert.jsx
@@ -6,6 +6,8 @@ import Loading from "../Loading/Loading";
 import { useState, useEffect } from "react";
 import Table from "./Table";
 import Pagination from './Pagination';
+import add from "./assets/add.svg";
+import s from "./assets/search.svg";
 
 const DisplayAllCert = ({ allCert }) => {
 
@@ -33,7 +35,72 @@ const DisplayAllCert = ({ allCert }) => {
   return (
     <>
       <div class="wap">
-        <div className="input-box">
+        <div className="upper-div">
+          <div className="tag-div">
+            <span class="cer1">
+              Certificates
+            </span>
+            <h1 class="cer2">
+              Certificates
+            </h1>
+          </div>
+          <div className="issuer-div">
+            <button className="issuer-btn">
+              <span>
+                <img src={add} alt="" />
+              </span>
+              <span>Issue Certificate</span>
+            </button>
+          </div>
+        </div>
+        <div className="lower-div">
+          <div className="filter-div">
+            <div className="search-div">
+              <h5>
+                <span>
+                  <img src={s} alt="" />
+                </span>
+                Search
+              </h5>
+              <div className="input-box">
+                <input type="text" class="search" onChange={event => setQuery(event.target.value.toLowerCase())} />
+                <span>Search by name</span>
+              </div>
+            </div>
+            <div className="hr-div"></div>
+            <div className="search-div">
+              <div className="filter-header">
+                <h5>
+                  <span>
+                    <img src={s} alt="" />
+                  </span>
+                  Filter
+                </h5>
+                <button class="filter-btn">
+                  <span>Clear all Filters</span>
+                </button>
+
+              </div>
+
+              <div className="input-box">
+                <input type="text" class="search" onChange={event => setQuery(event.target.value.toLowerCase())} />
+                <span>Select a group</span>
+              </div>
+            </div>
+          </div>
+          <div className="table-div">
+            <div className="table-btn">
+            <h5>1 Certificate
+              </h5>
+              <button disabled>Resend Email</button>
+            </div>
+            <div className="hr2"></div>
+            <div className="tab">
+            <Table data={search()} />
+            </div>
+          </div>
+        </div>
+        {/* <div className="input-box">
           <input type="text" class="search" onChange={event => setQuery(event.target.value.toLowerCase())} />
           <span>Search by name</span>
         </div>
@@ -46,7 +113,7 @@ const DisplayAllCert = ({ allCert }) => {
           />
         ) : (
           <></>
-        )}
+        )} */}
 
       </div>
     </>
