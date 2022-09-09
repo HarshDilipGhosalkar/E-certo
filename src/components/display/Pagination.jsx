949df1fe36ea6c1d39699a8f543becec9fe45bf1
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2022-09-09 13:13:44 +0530

    updated pagination

--- src/components/display/Pagination.jsx
+++ src/components/display/Pagination.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
-  const pageNumbers = [];
+  
+  const [page,setPage]=useState(1);
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
+  function pagination(decision){
+    var p=0;
+    if (decision=="inc") {
+    p=page+1;
+    setPage(p);
+    } else {
+    p=page-1;
+    setPage(p);  
+    }
+    dopainate(p);
   }
-
+  function dopainate(pno){
+    paginate(pno);
+  }
   return (
     <nav>
       <ul className='pagination'>
-        {pageNumbers.map(number => (
-          <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} className='page-link'>
-              {number}
-            </a>
+      <li className='page-item'>
+        {page <= 1 ? (
+          <></>
+        ) : (
+          <a onClick={() => pagination("dec")} className='page-link'>
+          Prev
+        </a>
+        )}
           </li>
-        ))}
+          <li className='page-item'>
+
+          <a  className='page-link'>
+          {page}
+        </a>
+
+          </li>
+          <li className='page-item'>
+        {page >= Math.ceil(totalPosts / postsPerPage) ? (
+          <></>
+        ) : (
+          <a onClick={() => pagination("inc")} className='page-link'>
+          inc
+        </a>
+        )}
+          </li>
+        
       </ul>
     </nav>
   );
