f6c01d4a25cf19825b932784a99f06e0706338aa
Jhenil Parihar <jhenilparihar@gmail.com>
2022-09-09 13:06:37 +0530

    change path of CreateFromExel component

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ import DisplayAllCert from "./display/displayAllCert";
 import StudentDetail from "./StudentDetails/StudentDetail";
 import DisplayCert from "./display/displaycert";
 import FormComponent from "./create-form/FormComponent";
-import CreateFromExel from "./create-form/uploadexel";
+import CreateFromExel from "./uploadExcel/uploadexel";
 import Query from "./Query/Query";
 import emailjs from "emailjs-com";
 import crypto from "crypto-js";
