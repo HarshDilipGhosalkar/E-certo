6623774ce2408aae31a4fe27b520da394b7c6775
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2022-09-10 13:23:51 +0530

    disabled

--- src/components/display/Pagination.jsx
+++ src/components/display/Pagination.jsx
@@ -37,7 +37,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
           </li>
           <li className='page-item'>
 
-          <button className='page-link'>
+          <button disabled className='page-link'>
           {page}
         </button>
 
