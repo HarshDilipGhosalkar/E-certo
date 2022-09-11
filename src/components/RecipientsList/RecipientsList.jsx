cf06f57f231093428e2d56cc833a63d4715504d4
Jhenil Parihar <jhenilparihar@gmail.com>
2022-09-11 17:25:51 +0530

    added delete row functionaity

--- src/components/RecipientsList/RecipientsList.jsx
+++ src/components/RecipientsList/RecipientsList.jsx
@@ -81,21 +81,21 @@ class RecipientsList extends Component {
                     <div className="col">
                       <button className="reupload_btn">
                         <span>
-                        <svg
-                          viewBox="64 64 896 896"
-                          focusable="false"
-                          data-icon="plus"
-                          width="1em"
-                          height="1em"
-                          fill="currentColor"
-                          aria-hidden="true"
-                        >
-                          <defs>
-                            <style></style>
-                          </defs>
-                          <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z"></path>
-                          <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z"></path>
-                        </svg>
+                          <svg
+                            viewBox="64 64 896 896"
+                            focusable="false"
+                            data-icon="plus"
+                            width="1em"
+                            height="1em"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <defs>
+                              <style></style>
+                            </defs>
+                            <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z"></path>
+                            <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z"></path>
+                          </svg>
                         </span>
                         Add Recipients
                       </button>
@@ -134,10 +134,10 @@ class RecipientsList extends Component {
                     </div>
                     <div className="action-area">
                       <div className="action-head">Action</div>
-                      {this.state.excelData.map(() => {
+                      {this.state.excelData.map((data) => {
                         return (
                           <div className="action-icon">
-                            <button className="action-btn">
+                            <button name="edit" className="action-btn">
                               <span
                                 role="img"
                                 aria-label="edit"
@@ -164,7 +164,20 @@ class RecipientsList extends Component {
                                 </svg>
                               </span>
                             </button>
-                            <button className="action-btn">
+                            <button
+                              onClick={() => {
+                                console.log(data.SAP);
+                                var excelData = this.state.excelData;
+                                var index = this.state.excelData.indexOf(data);
+                                // console.log(index)
+                                if (index > -1) {
+                                  excelData.splice(index, 1);
+                                  this.setState({excelData: excelData})
+                                }
+                              }}
+                              name="delete"
+                              className="action-btn"
+                            >
                               <span
                                 role="img"
                                 aria-label="delete"
