31cc60921d7612a7960f4d5597291e476ccc1af4
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2022-09-11 21:11:15 +0530

    formatted

--- src/components/display/Table.jsx
+++ src/components/display/Table.jsx
@@ -1,125 +1,87 @@
 import React from "react";
-import { useState, useEffect,useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const Table = ({ allCert,data,enableState,disableState,emailLoop}) => {
-    // const [clicked,setchecked]=useState(resendStatus);
+const Table = ({ allCert, data, enableState, disableState, emailLoop }) => {
     const resendStatus = useRef(false);
-    var emailList=[];
-    var checked=0;
-
-    // console.log("hook",clicked);
+    var emailList = [];
+    var checked = 0;
     function arrayRemove(arr, value) {
- 
-        return arr.filter(function(cer){
+
+        return arr.filter(function (cer) {
             return cer.certid.toNumber() != value;
         });
-      
-     }
-    const send=(cert,clsname)=>{
-        var checkBox = document.querySelector("."+clsname);
-        if (checkBox.checked == true){
-            
+
+    }
+    const send = (cert, clsname) => {
+        var checkBox = document.querySelector("." + clsname);
+        if (checkBox.checked == true) {
+
             emailList.push(cert);
-            checked+=1;
+            checked += 1;
             enableState();
-          } else {
-             
+        } else {
+
             emailList = arrayRemove(emailList, cert.certid.toNumber());
-            checked-=1;
-            if(checked<=0){
+            checked -= 1;
+            if (checked <= 0) {
                 disableState();
             }
-          }
+        }
 
         console.log(emailList);
-        
+
     }
-    function tog(){
-        // resendStatus="true";
+    function tog() {
+
         resendStatus.current = true;
         console.log(resendStatus.current);
         emailLoop(emailList);
         // window.location.reload();
-      }
-    // useEffect(() => {
-    //     if(clicked==true){
-    //         console("yes I got it");
-    //     }
-    //   },[clicked]);
+    }
+
     return (
         <>
-        <div className="table-btn">
-            <h5>{allCert.length} Certificates
-              </h5>
-              <div id="resend-btn" onClick={()=>tog()} >Resend Email</div>
+            <div className="table-btn">
+                <h5>{allCert.length} Certificates
+                </h5>
+                <div id="resend-btn" onClick={() => tog()} >Resend Email</div>
             </div>
             <div className="hr2"></div>
             <div className="tab">
-            {data !== undefined ? (
-            <>
-        <table class="content-table">
-            <thead>
-                <tr>
-                <th><input type="checkbox" /></th>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Department</th>
-                    <th>Issue Date</th>
-                </tr>
-            </thead>
-            <tbody>
-            
-                {data.map((item) => (
-                    <tr >
-                        <td><input type="checkbox" className={"chk"+item.certid.toNumber()} onClick={() => send(item,"chk"+item.certid.toNumber())}/></td>
-                    <a href={"certificate/" + item.transactionHash}><td>{item.certid.toNumber()}</td></a>
-                                <td>{item.name}</td>
-                                <td>{item.email}</td>
-                                <td>{item.course}</td>
-                                <td>{item.issueDate}</td>
-                               
-        
-                            </tr>
-                ))}
+                {data !== undefined ? (
+                    <>
+                        <table class="content-table">
+                            <thead>
+                                <tr>
+                                    <th><input type="checkbox" /></th>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Department</th>
+                                    <th>Issue Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-            </tbody>
-        </table>
-        </>
-         ) : null}
+                                {data.map((item) => (
+                                    <tr >
+                                        <td><input type="checkbox" className={"chk" + item.certid.toNumber()} onClick={() => send(item, "chk" + item.certid.toNumber())} /></td>
+                                        <a href={"certificate/" + item.transactionHash}><td>{item.certid.toNumber()}</td></a>
+                                        <td>{item.name}</td>
+                                        <td>{item.email}</td>
+                                        <td>{item.course}</td>
+                                        <td>{item.issueDate}</td>
+
+
+                                    </tr>
+                                ))}
+
+                            </tbody>
+                        </table>
+                    </>
+                ) : null}
             </div>
-        {/* {data !== undefined ? (
-            <>
-        <table class="content-table">
-            <thead>
-                <tr>
-                <th><input type="checkbox" /></th>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Department</th>
-                    <th>Issue Date</th>
-                </tr>
-            </thead>
-            <tbody>
-            
-                {data.map((item) => (
-                    <tr >
-                        <td><input type="checkbox" className={"chk"+item.certid.toNumber()} onClick={() => send(item,"chk"+item.certid.toNumber())}/></td>
-                    <a href={"certificate/" + item.transactionHash}><td>{item.certid.toNumber()}</td></a>
-                                <td>{item.name}</td>
-                                <td>{item.email}</td>
-                                <td>{item.course}</td>
-                                <td>{item.issueDate}</td>
-                               
-        
-                            </tr>
-                ))}
 
-            </tbody>
-        </table>
-        </>
-         ) : null} */}
         </>
     );
 };
