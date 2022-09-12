42950eeebd696153e03c532218226c5c4f8b1e46
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2022-09-12 20:21:22 +0530

    converted functional component to class component

--- src/components/display/Table.jsx
+++ src/components/display/Table.jsx
@@ -1,107 +1,125 @@
-import React from "react";
+import React, { Component } from "react";
 import { useState, useEffect, useRef } from "react";
 
-const Table = ({ allCert, data, enableState, disableState, emailLoop }) => {
-    // const resendStatus = useRef(false);
-    const [check, setcheck] = useState(0);
-    var emailList = [];
-    var checked = 0;
+class Table extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        emailList: [],
+        checked: 0,
+        check: 0,
+        index:0,
+      };
+    }
+   arrayRemove= async (arr, value) =>  {
 
-    function arrayRemove(arr, value) {
-
-        return arr.filter(function (cer) {
-            return cer.certid.toNumber() != value;
-        });
+        // return arr.filter(function (cer) {
+        //     return cer.certid.toNumber() != value;
+        // });
 
     }
-    const send = (cert, clsname) => {
+    send =async (cert, clsname) => {
         var checkBox = document.querySelector(".chk" + clsname);
         var tr = document.querySelector(".tr" + clsname);
         if (checkBox.checked == true) {
             tr.style.backgroundColor = '#e6f7ff';
-            emailList.push(cert);
-            checked += 1;
-            enableState();
-            if (checked == data.length) {
+            await this.setState({emailList:[...this.state.emailList,cert]})
+            await this.setState({checked: this.state.checked+1})
+            this.props.enableState();
+            if (this.state.checked == this.props.data.length) {
                 document.querySelector(".allCheck").checked = true;
             } else {
                 document.querySelector(".allCheck").checked = false;
             }
+            console.log("checked")
         } else {
-
-            emailList = arrayRemove(emailList, cert.certid.toNumber());
-            checked -= 1;
-            if (checked <= 0) {
-                disableState();
+            var slicedData = this.state.emailList;
+            
+            let index = slicedData.indexOf(cert);
+            console.log(index);
+            
+            if (index > -1) { 
+                slicedData.splice(index, 1); 
+              }
+            await this.setState({emailList:slicedData});
+            await this.setState({checked: this.state.checked-1})
+            if (this.state.checked <= 0) {
+                this.props.disableState();
             }
             tr.style.backgroundColor = 'white';
             document.querySelector(".allCheck").checked = false;
+            // console.log("unchecked")
         }
 
-        console.log(emailList);
-        // setcheck(checked);
+        console.log(this.state.emailList);
+        // console.log(this.state.checked);
+        
     }
-    function tog() {
-        const trs = document.querySelectorAll(".commontr");
-        const checkBoxes = document.querySelectorAll(".commonChk");
-        const allCheck = document.querySelector(".allCheck");
-        if (allCheck.checked == true) {
-            console.log("yes");
-            emailList = []
-            emailList = [...data];
-            checkBoxes.forEach(chk => {
-                chk.checked = true;
-            });
-            trs.forEach(tr => {
-                tr.style.backgroundColor = '#e6f7ff';
-            });
-            console.log(emailList);
-            enableState();
-        } else {
-            emailList = []
-            checkBoxes.forEach(chk => {
-                chk.checked = false;
-            });
-            trs.forEach(tr => {
-                tr.style.backgroundColor = 'white';
-            });
-            console.log(emailList);
-            disableState();
-        }
+    tog= async () =>  {
+        // this.setState({emailList:[]})
+        // const trs = document.querySelectorAll(".commontr");
+        // const checkBoxes = document.querySelectorAll(".commonChk");
+        // const allCheck = document.querySelector(".allCheck");
+        // if (allCheck.checked == true) {
+        //     console.log("yes");
+        //     const data=this.props.data
+        //     data.forEach(certi => {
+        //         this.setState({emailList:[...this.state.emailList,certi]})
+        //     });
+        //     checkBoxes.forEach(chk => {
+        //         chk.checked = true;
+        //     });
+        //     trs.forEach(tr => {
+        //         tr.style.backgroundColor = '#e6f7ff';
+        //     });
+            
+        //     console.log(this.state.emailList);
+        //     this.props.enableState();
+        // } else {
+        //     this.setState({emailList:[]})
+        //     checkBoxes.forEach(chk => {
+        //         chk.checked = false;
+        //     });
+        //     trs.forEach(tr => {
+        //         tr.style.backgroundColor = 'white';
+        //     });
+        //     console.log(this.state.emailList);
+        //     this.props.disableState();
+        // }
 
     }
-
-    return (
+    render() {
+      return (
         <>
             <div className="table-btn">
-                {data.length <= 1 ? (
+                {this.props.data.length <= 1 ? (
                     <>
-                        <h5>{data.length} Certificate
+                        <h5>{this.props.data.length} Certificate
                         </h5>
                     </>
                 ) : (
                     <>
-                        <h5>{data.length} Certificates
+                        <h5>{this.props.data.length} Certificates
                         </h5></>
                 )}
 
-                <div id="resend-btn" onClick={() => emailLoop(emailList)} >Resend Email</div>
+                <div id="resend-btn" onClick={() => this.props.emailLoop(this.state.emailList)} >Resend Email</div>
             </div>
             <div className="hr2"></div>
             <div className="tab">
-                {data !== undefined ? (
+                {this.props.data !== undefined ? (
                     <>
                         <table class="content-table">
                             <thead>
                                 <tr>
-                                    {data.length != 0 ? (
+                                    {this.props.data.length != 0 ? (
                                         <>
-                                            <th><input type="checkbox" className="allCheck" onClick={() => tog()} /></th>
+                                            <th><input type="checkbox" className="allCheck" onClick={() => this.tog()} /></th>
 
                                         </>
                                     ) : (
                                         <>
-                                            <th><input type="checkbox" className="allCheck" onClick={() => tog()} disabled /></th>
+                                            <th><input type="checkbox" className="allCheck" onClick={() => this.tog()} disabled /></th>
 
                                         </>
                                     )}
@@ -114,9 +132,9 @@ const Table = ({ allCert, data, enableState, disableState, emailLoop }) => {
                             </thead>
                             <tbody>
 
-                                {data.map((item) => (
+                                {this.props.data.map((item) => (
                                     <tr className={"commontr tr" + item.certid.toNumber()}>
-                                        <td><input type="checkbox" className={"commonChk  chk" + item.certid.toNumber()} onClick={() => send(item, item.certid.toNumber())} /></td>
+                                        <td><input type="checkbox" className={"commonChk  chk" + item.certid.toNumber()} onClick={() => this.send(item, item.certid.toNumber())} /></td>
                                         <td><a href={"certificate/" + item.transactionHash}>{item.certid.toNumber()}</a></td>
                                         <td>{item.name}</td>
                                         <td>{item.email}</td>
@@ -134,7 +152,8 @@ const Table = ({ allCert, data, enableState, disableState, emailLoop }) => {
             </div>
 
         </>
-    );
-};
+      );
+    }
+  }
 
 export default Table;
